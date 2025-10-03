36998e7024b6e89f682e891b5077396b1c4cd568
あさと <sethuna03@gmail.com>
2025-10-03 13:02:26 +0900

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import mdx from '@mdx-js/rollup'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@mdx-js/rollup";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     mdx({
-      providerImportSource: '@mdx-js/react',
+      providerImportSource: "@mdx-js/react",
     }),
     react(),
     tailwindcss(),
   ],
-})
+});
